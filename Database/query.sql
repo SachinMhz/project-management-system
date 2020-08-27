e0ceabc0d0f7570e9c837ac2e872f09d4c765263
SachinMhz <sachin.mhz.sm@gmail.com>
2020-08-27 13:21:35 +0545

    add readme

---
+++ Database/query.sql
@@ -0,0 +1,77 @@
+CREATE DATABASE project;
+
+\c project
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY key NOT NULL,
+    display_name varchar(50) NOT NULL,
+    email  varchar(50) UNIQUE NOT NULL,
+    role varchar(50) NOT NULL,
+    hash varchar NOT NULL
+);
+
+CREATE TABLE projects (
+    project_id SERIAL PRIMARY key,
+    name varchar(50) UNQIUE NOT NULL,
+    description varchar(250) NOT NULL,
+    manager_id integer NOT NULL,
+    FOREIGN KEY (manager_id) 
+	  REFERENCES users(user_id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE user_on_project (
+    id SERIAL PRIMARY KEY,
+    project_id integer NOT NULL,
+    user_id integer NOT NULL,
+    FOREIGN KEY (project_id) 
+	  REFERENCES projects(project_id)
+      ON DELETE CASCADE,
+    FOREIGN KEY (user_id) 
+	  REFERENCES users(user_id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE tasks(
+    task_id SERIAL PRIMARY key,
+    project_id integer NOT NULL,
+    user_id integer,
+    previous_user_id integer,
+    title varchar NOT NULL,
+    description varchar(250) NOT NULL,
+    deadline date NOT NULL,
+    FOREIGN KEY (project_id) 
+	  REFERENCES projects(project_id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE comments(
+    comment_id SERIAL PRIMARY key,
+    task_id integer NOT NULL,
+    commenter_id integer NOT NULL,
+    commenter_name varchar(50) NOT NULL,
+    comment varchar(250) NOT NULL,
+    comment_time TIME DEFAULT CURRENT_TIME,
+    FOREIGN KEY (task_id) 
+	  REFERENCES tasks(task_id)
+      ON DELETE CASCADE,
+    FOREIGN KEY (commenter_id) 
+	  REFERENCES users(user_id)
+      ON DELETE SET NULL
+);
+
+CREATE TABLE tags(
+    tag_id SERIAL PRIMARY key,
+    task_id integer NOT NULL,
+    tagger_id integer NOT NULL,
+    tagged_id integer NOT NULL,
+    FOREIGN KEY (task_id) 
+	  REFERENCES tasks(task_id)
+      ON DELETE CASCADE,
+    FOREIGN KEY (tagger_id) 
+	  REFERENCES users(user_id)
+      ON DELETE CASCADE,
+    FOREIGN KEY (tagged_id) 
+	  REFERENCES users(user_id)
+      ON DELETE CASCADE
+);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # project-management-system
+
+# steps
+1. clone this repository
+2. In psql cmd type following command:
+    a. \i '<cloned_project_path>/Database/query.sql'
+3. In BackEnd folder :
+    a. add you environment variable from .env.example to .env
+    b. add JWT_SECRET_TOKEN from the email
+    c. yarn install or npm install
+    d. yarn start or npm start
+
+3. In FrontEnd folder :
+    a. yarn install or npm install
+    b. yarn start or npm start
+
+4. Go to "http://localhost:8080/" in browser
+
+login information in the email.
